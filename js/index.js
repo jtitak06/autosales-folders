ab397e35eff4005f41184c0e9b394d8c210227f3
Jeff Titak <jefftitak@Jeffs-MacBook-Air.local>
2024-04-16 16:51:07 +0300

    Updated Search filter

--- js/index.js
+++ js/index.js
@@ -32,11 +32,10 @@ document.addEventListener('DOMContentLoaded', async function() {
   const currentMakeSelect = document.getElementById('currentMake');
   const currentModelSelect = document.getElementById('currentModel');
 
-  // Store default values from HTML
-  const defaultYearFrom = yearFromSelect.getAttribute('value');
-  const defaultYearTo = yearToSelect.getAttribute('value');
-  const defaultMake = currentMakeSelect.getAttribute('value');
-  const defaultModel = currentModelSelect.getAttribute('value');
+  // Set default values programmatically
+  const defaultYearValue = 'Select Year';
+  const defaultMakeValue = 'Select Make';
+  const defaultModelValue = 'Select Model';
 
   let years = [];
 
@@ -51,26 +50,36 @@ document.addEventListener('DOMContentLoaded', async function() {
     const makes = Array.from(makesSet).sort();
     const models = Array.from(modelsSet).sort();
 
-    populateDropdown(yearFromSelect, years, defaultYearFrom);
-    populateDropdown(yearToSelect, years, defaultYearTo);
-    populateDropdown(currentMakeSelect, makes, defaultMake);
-    populateDropdown(currentModelSelect, models, defaultModel);
+    // Populate dropdowns with dynamic data
+    populateDropdown(yearFromSelect, years, defaultYearValue, 'Select Year');// Set default text for Years
+    populateDropdown(yearToSelect, years, defaultYearValue, 'Select Year');// Set default text for Years
+    populateDropdown(currentMakeSelect, makes, defaultMakeValue, 'Select Make'); // Set default text for Makes
+    populateDropdown(currentModelSelect, models, defaultModelValue, 'Select Model'); // Set default text for Models
 
     // Set default values for select elements from HTML
-    yearFromSelect.value = defaultYearFrom;
-    yearToSelect.value = defaultYearTo;
-    currentMakeSelect.value = defaultMake;
-    currentModelSelect.value = defaultModel;
+    yearFromSelect.value = defaultYearValue;
+    yearToSelect.value = defaultYearValue;
+    currentMakeSelect.value = defaultMakeValue;
+    currentModelSelect.value = defaultModelValue;
+
+    // Ensure "Select" is selected after populating the dropdowns
+    yearFromSelect.value = 'Select Year';
+    yearToSelect.value = 'Select Year';
+    currentMakeSelect.value = 'Select Make';
+    currentModelSelect.value = 'Select Model';
 
     // Store selected yearFrom and yearTo values
-    let selectedYearFrom = defaultYearFrom;
-    let selectedYearTo = defaultYearTo;
+    let selectedYearFrom = defaultYearValue;
+    let selectedYearTo = defaultYearValue;
+
+    console.log('selected yearFrom', selectedYearFrom);
+    console.log('selected YearTo', selectedYearTo);
 
     // Event listener to update yearTo options based on selected yearFrom
     yearFromSelect.addEventListener('change', function() {
       selectedYearFrom = yearFromSelect.value;
       const availableYearsTo = years.filter(year => year >= selectedYearFrom || year === '');
-      populateDropdown(yearToSelect, availableYearsTo);
+      populateDropdown(yearToSelect, availableYearsTo, '', 'Select Year'); // Add "Select Year" option
       yearToSelect.value = selectedYearTo; // Restore selected yearTo value
     });
 
@@ -78,7 +87,7 @@ document.addEventListener('DOMContentLoaded', async function() {
     yearToSelect.addEventListener('change', function() {
       selectedYearTo = yearToSelect.value;
       const availableYearsFrom = years.filter(year => year <= selectedYearTo || year === '');
-      populateDropdown(yearFromSelect, availableYearsFrom);
+      populateDropdown(yearFromSelect, availableYearsFrom, '', 'Select Year'); // Add "Select Year" option
       yearFromSelect.value = selectedYearFrom; // Restore selected yearFrom value
     });
   } catch (error) {
@@ -86,104 +95,73 @@ document.addEventListener('DOMContentLoaded', async function() {
   }
 });
 
-fetch('https://sheetdb.io/api/v1/gfqcm38bwhrl4')
-  .then(response => response.json())
-  .then(data => {
-    const carousel = document.getElementById('carousel');
+// Function to populate dropdown options
+function populateDropdown(selectElement, options, defaultValue, defaultText) {
+  // Clear existing options
+  selectElement.innerHTML = '';
 
-    // Iterate through each item in the data and create a card for it
-    data.forEach(item => {
-      const card = document.createElement('div');
-      card.className = 'grid-card';
+  // Add default option
+  const defaultOption = document.createElement('option');
+  defaultOption.textContent = defaultText;
+  defaultOption.disabled = true; // Disable the default option
+  selectElement.appendChild(defaultOption);
 
-      // Create image element
-      const imageWrapper = document.createElement('a');
-      imageWrapper.className = 'grid-image-wrapper';
-      imageWrapper.href = `/vehicle/${item.vin}`;
-      imageWrapper.addEventListener('click', newLink);
+  // Add dynamic options
+  options.forEach(option => {
+    const optionElement = document.createElement('option');
+    optionElement.value = option;
+    optionElement.textContent = option;
+    selectElement.appendChild(optionElement);
+  });
 
-      const image = document.createElement('img');
-      image.className = 'grid-image';
-      image.src = item.imageURL !== '' ? item.imageURL : 'NoImage';
-      image.alt = 'grid';
-      imageWrapper.appendChild(image);
+  // Set default value
+  if (defaultValue || defaultValue === '') {
+    selectElement.value = defaultValue;
+  } else {
+    selectElement.value = ''; // Set default value to empty string if defaultValue is not provided
+  }
+}
 
-      // Create info section
-      const infoSection = document.createElement('div');
-      infoSection.className = 'grid-info-section';
+/* document.addEventListener("DOMContentLoaded", function() {
+  const carousel = document.getElementById('carousel');
+  const prevBtn = document.getElementById('prevBtn');
+  const nextBtn = document.getElementById('nextBtn');
 
-      const title = document.createElement('h4');
-      title.className = 'grid-title';
-      title.textContent = `${item.make} ${item.model}`;
+  let currentIndex = 0;
 
-      const year = document.createElement('p');
-      year.className = 'grid-text';
-      year.innerHTML = `<span class="grid-text-title">Year: </span>${item.year}`;
+  prevBtn.addEventListener('click', function() {
+      const cardWidth = carousel.children[0].offsetWidth;
+      const cardMargin = parseInt(window.getComputedStyle(carousel.children[0]).marginRight);
 
-      const mileage = document.createElement('p');
-      mileage.className = 'grid-text';
-      mileage.innerHTML = `<span class="grid-text-title">Mileage: </span>${item.mileage.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} miles`;
+      // Calculate translation value to shift one card width to the right
+      const shift = -(cardWidth + cardMargin);
 
-      // Create list wrapper
-      const listWrapper = document.createElement('ul');
-      listWrapper.className = 'grid-list-wrapper';
-
-      // Check if item.internetPrice exists before accessing its properties
-      if (item.internetPrice) {
-        const priceItem = document.createElement('li');
-        priceItem.className = 'grid-list-item';
-
-        const price = document.createElement('p');
-        price.className = 'grid-price';
-        price.textContent = `$${item.internetPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
-        priceItem.appendChild(price);
-
-        listWrapper.appendChild(priceItem);
-      }
-
-      const wishlistItem = document.createElement('li');
-      wishlistItem.className = 'grid-list-item';
-
-      const wishlistButton = document.createElement('button');
-      wishlistButton.className = 'grid-wishlist';
-      wishlistButton.addEventListener('click', () => {
-        selected?.wishlist ? removeFromList(item.vin) : addToList(item.vin);
+      // Shift each card to the right
+      Array.from(carousel.children).forEach((card, index) => {
+          const newPosition = parseInt(card.style.left || 0) + shift;
+          card.style.left = newPosition + 'px';
       });
 
-      const wishlistIcon = document.createElement('FontAwesomeIcon');
-      wishlistIcon.className = 'grid-wishlist-icon';
-      wishlistIcon.icon = 'faHeart';
-      wishlistButton.appendChild(wishlistIcon);
+      // Update currentIndex
+      currentIndex = Math.max(currentIndex - 1, 0);
+  });
 
-      wishlistItem.appendChild(wishlistButton);
+  nextBtn.addEventListener('click', function() {
+      const cardWidth = carousel.children[0].offsetWidth;
+      const cardMargin = parseInt(window.getComputedStyle(carousel.children[0]).marginRight);
 
-      // Append elements
-      card.appendChild(imageWrapper);
-      card.appendChild(infoSection);
-      infoSection.appendChild(title);
-      infoSection.appendChild(year);
-      infoSection.appendChild(mileage);
-      infoSection.appendChild(listWrapper);
-      listWrapper.appendChild(wishlistItem);
+      // Calculate translation value to shift one card width to the left
+      const shift = cardWidth + cardMargin;
 
-      carousel.appendChild(card);
-    });
-  })
-  .catch(error => console.error('Error fetching data:', error));
+      // Shift each card to the left
+      Array.from(carousel.children).forEach((card, index) => {
+          const newPosition = parseInt(card.style.left || 0) + shift;
+          card.style.left = newPosition + 'px';
+      });
 
-// Function for new link
-function newLink(event) {
-  // Add your new link logic here
-}
-
-// Function to add to wishlist
-function addToList(vin) {
-  // Add to wishlist logic
-}
-
-// Function to remove from wishlist
-function removeFromList(vin) {
-  // Remove from wishlist logic
-}
+      // Update currentIndex
+      currentIndex = Math.min(currentIndex + 1, carousel.children.length - 1);
+  });
+}); */
 
 
